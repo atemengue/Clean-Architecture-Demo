26328d270dbf631be4bfc7b9b67bf1b1c769a327
Régis Atemengue <moafembe@gmail.com>
2026-03-30 16:42:46 -0500

    Ajoutez des fichiers projet.

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Category : AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Event>? Events { get; set; }
+    }
+}
---
+++ Domain/Entities/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlave { get; set; }
+        public bool OrderPaid { get; set; }
+
+    }
+}
