215c2b1df5ef3cbfba65bdc73c44d0ad463d7acd
Régis Atemengue <moafembe@gmail.com>
2026-03-30 16:59:23 -0500

    feat: add Event DTO's

---
+++ Application/DTOs/Event/CreateEventDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.Event
+{
+    public class CreateEventDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Price { get; set; }
+        public string? Artist { get; set; }
+        public DateTime Date { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
